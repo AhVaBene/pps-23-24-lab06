3fe6e5f4b58f00365a09401622659795753c51a4
Riccardo Alni - riccardo.alni@studio.unibo.it <riccardo.alni@studio.unibo.it>
2024-04-15 12:02:16 +0200

    ex 1

--- src/main/scala/ex1/OOFPList.scala
+++ src/main/scala/ex1/OOFPList.scala
@@ -13,6 +13,7 @@ enum List[A]:
   def tail: Option[List[A]] = this match
     case h :: t => Some(t)
     case _ => None
+
   def foreach(consumer: A => Unit): Unit = this match
     case h :: t => consumer(h); t.foreach(consumer)
     case _ =>
@@ -45,13 +46,21 @@ enum List[A]:
     case h :: t => t.foldLeft(h)(op)
 
   // Exercise: implement the following methods
-  def zipWithValue[B](value: B): List[(A, B)] = ???
-  def length(): Int = ???
-  def zipWithIndex: List[(A, Int)] = ???
-  def partition(predicate: A => Boolean): (List[A], List[A]) = ???
-  def span(predicate: A => Boolean): (List[A], List[A]) = ???
-  def takeRight(n: Int): List[A] = ???
-  def collect(predicate: PartialFunction[A, A]): List[A] = ???
+  def zipWithValue[B](value: B): List[(A, B)] = flatMap(a => (a, value) :: Nil()) //foldLeft(Nil())((b, a) => b append (a, value) :: Nil())
+  def length(): Int = foldLeft(0)((b, a) => b + 1)
+  def zipWithIndex: List[(A, Int)] = foldRight(Nil())((a, b) => (a, this.length() - b.length() - 1) :: Nil() append b)
+  def partition(predicate: A => Boolean): (List[A], List[A]) = foldRight((Nil(), Nil()))((a, b) => 
+    if predicate(a) then (a :: Nil() append b._1, b._2) else (b._1, a :: Nil() append b._2)) //(filter(predicate), filter(a => !predicate(a)))
+  def span(predicate: A => Boolean): (List[A], List[A]) = foldRight((Nil(), Nil()))((a, b) => 
+    if (predicate(a) && b._2.length() > 1) then (a :: Nil() append b._1, b._2) else (b._1, a :: Nil() append b._2))
+  def takeRight(n: Int): List[A] = foldLeft((Nil(): List[A], Nil(): List[A]))((b, a) => 
+    if(this.length() - b._2.length() == n) then (b._1 append a :: Nil(), b._2) else (b._1, b._2 append a :: Nil()))._1
+    /*this match
+    case h :: t if t.length() == n => t
+    case h :: t => t.takeRight(n)
+    case _ => Nil()*/
+
+  def collect(predicate: PartialFunction[A, A]): List[A] = filter(predicate.isDefinedAt(_)).map(predicate)
 // Factories
 object List:
 
@@ -67,12 +76,14 @@ object Test extends App:
 
   import List.*
   val reference = List(1, 2, 3, 4)
+  println(reference)
   println(reference.zipWithValue(10)) // List((1, 10), (2, 10), (3, 10), (4, 10))
   println(reference.zipWithIndex) // List((1, 0), (2, 1), (3, 2), (4, 3))
   println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
   println(reference.span(_ % 2 != 0)) // (List(1), List(2, 3, 4))
   println(reference.span(_ < 3)) // (List(1, 2), List(3, 4))
-  println(reference.reduce(_ + _)) // 10
-  println(List(10).reduce(_ + _)) // 10
+  //println(reference.reduce(_ + _)) // 10
+  //println(List(10).reduce(_ + _)) // 10
   println(reference.takeRight(3)) // List(2, 3, 4)
   println(reference.collect { case x if x % 2 == 0 => x + 1 }) // List(3, 5)
+  println(reference.collect { case x if x % 2 != 0 => x * 2 }) // List(2, 6)
